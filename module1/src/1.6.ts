a9c1b1a18d8e9b4ea2d1bbebda2397f8c81787d2
Md. Salauddin <salauddinahmed555@gmail.com>
2024-10-31 00:00:55 +0600

    Functions in ts

---
+++ module1/src/1.6.ts
@@ -0,0 +1,28 @@
+// Learnig function
+// Normal function
+// Arrow function
+
+
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+add(1, 5)
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+
+// object ---> function ---> method
+
+const poorUser = {
+    name: 'Mejba',
+    balance: 0,
+    addBalance(balance: number): string{
+        return `My new balance is: ${this.balance + balance}`;
+    }
+} 
+
+
+const arr : number[]= [1,4,10]
+
+const newArray: number[] = arr.map((elem: number): number => elem*elem)
