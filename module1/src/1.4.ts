bdea2d4d403e487ebb82af6f5c8c270ed5ac4e4d
Md. Salauddin <salauddinahmed555@gmail.com>
2024-10-30 23:11:25 +0600

    explored basic types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,49 @@
+// basic data type
+
+// string
+
+// explicit data type
+let firstname : string = 'mezba';
+
+// num
+let roll: number = 123;
+
+//boolean 
+let isAdmin: boolean = false;
+
+// undefined
+let x: undefined;
+
+// null
+let y: null = null;
+
+
+let d; // similar to any
+
+d = 123 
+d= 'fdg'
+d = true
+
+// array 
+
+let friends = ['rachel', 'monica']
+let eligibleRollList : number[] = [1,3]
+
+eligibleRollList.push('dd') // 's' not a to num 
+
+
+// tuple --> array --> order --> type of values
+
+let coordinate: [number, number] = [1, 5]
+
+let ageName: [number, string, boolean] = [50, 'Mr.X', false]
+
+
+ageName[0]= 65;
+ageName[1]= 'MR.Y';
+
+console.log(ageName[1])
+
+
+
+
