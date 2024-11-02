122a5aa0fb85f95bfa708f660536e796315e9b16
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-02 23:22:40 +0600

    comment added

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -1,43 +1,34 @@
-//generic constraints with keyof operator
-
-
+// Defining a Vehicle type with specific properties
 type Vehicle = {
     bike: string;
     car: string;
-    ship: string
-}
-type Owner = "bike" | "car" | "ship" ; //manually
+    ship: string;
+};
 
-type Owner2 = keyof Vehicle // through keyof
+// Manually creating a union type that matches Vehicle's property keys
+type Owner = "bike" | "car" | "ship";
 
-const  getPropertyValue = <X,Y extends keyof X
- >(obj: X, key : Y) => {
-return obj[key] 
-}
+// Using `keyof` operator to automatically derive a union type of Vehicle's keys
+type Owner2 = keyof Vehicle; // Equivalent to: "bike" | "car" | "ship"
 
+// Generic function `getPropertyValue` that retrieves a property value from an object
+const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    // Returns the value of the specified key in the object
+    return obj[key];
+};
 
-
+// Example object `ussser` with various properties (not used in function)
 const ussser = {
-    name :'mr. persian',
-    age:26,
-    address:'ctg'
-}
+    name: 'mr. persian',
+    age: 26,
+    address: 'ctg'
+};
 
+// Another example object `car` with properties for demonstration
 const car = {
     model: 'toyota 100',
     year: 300
-}
+};
 
-const rezult1 = getPropertyValue(car, 'model');
-
-// obj
-
-// const usar  = {
-//     name: 'Mr. Persian',
-//     age: 26,
-//     address: 'ctg'
-// }
-
-// usar['name'] 
-
-
+// Using `getPropertyValue` to retrieve the 'model' property from the `car` object
+const rezult1 = getPropertyValue(car, 'model'); // Returns: 'toyota 100'
