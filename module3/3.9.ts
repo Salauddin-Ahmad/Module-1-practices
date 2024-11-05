8314a0e5c7387bd8730d81ee33760388687f0f7e
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-06 01:18:37 +0600

    3-9 Abstraction in OOP

--- module3/3.9.ts
+++ module3/3.9.ts
@@ -2,13 +2,56 @@
 // 1. interface and abstract methods
 
 interface Vehicle1 {
-    name: string;
-    model: string;
+  startEngine(): void;
+  stopEngine(): void;
+  move(): void;
 }
 
-const Vehicle1 : Vehicle1 = {
-    name: 'Toyota',
-    model: '2020',
+// real implementation
+class Car implements Vehicle1 {
+  startEngine(): void {
+    console.log("I am starting the car engine");
+  }
+  stopEngine(): void {
+    console.log("Car engine started");
+  }
+  move(): void {
+    console.log(`I am moving the car`);
+  }
+
+  test() {
+    console.log(` I am just testing`);
+  }
 }
 
+const toyotaCar = new Car();
+toyotaCar.startEngine();
 
+// abstract class
+
+
+// idea 
+ abstract class Car2 {
+    abstract startEngine(): void ;
+    abstract stopEngine(): void ;
+  abstract move(): void ;
+
+  test() {
+    console.log(` I am just testing`);
+  }
+}
+ class ToyotaCar extends Car2 {
+    startEngine(): void {
+        console.log(`start engine`)
+    }
+    stopEngine(): void {
+        console.log(`stop engine`)
+    }
+    move(): void {
+        console.log('I am moving the  car')
+    }
+ }
+
+
+// const hondaCar = new Car2();
+// hondaCar.startEngine();
