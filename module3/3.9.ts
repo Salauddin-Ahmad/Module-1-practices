31e642c944489175fa6d3f7f417f891486296ad9
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-05 23:45:46 +0600

    3-9 Abstraction in OOP starting

---
+++ module3/3.9.ts
@@ -0,0 +1,14 @@
+// 3-9 Abstraction in OOP
+// 1. interface and abstract methods
+
+interface Vehicle1 {
+    name: string;
+    model: string;
+}
+
+const Vehicle1 : Vehicle1 = {
+    name: 'Toyota',
+    model: '2020',
+}
+
+
